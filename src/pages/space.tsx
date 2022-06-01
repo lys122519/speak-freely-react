fd2133a4403912f9dbc5cd706a97e094e39da5fc
aPlikethis <yourhand1025@163.com>
2022-06-01 15:47:18 +0800

    用户卡

--- src/pages/space.tsx
+++ src/pages/space.tsx
@@ -1,6 +1,7 @@
 import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
 import "./space.less";
+import UserSpaceCard from "./user_space_card";
 
 const { Header, Content } = Layout;
 
@@ -13,7 +14,7 @@ const UserSpace: React.FC = () => {
                     <div className="user-space-content">
                         <div className="left">
                             <div className="user-card">
-
+                                <UserSpaceCard/>
                             </div>
                         </div>
                         <div className="right">

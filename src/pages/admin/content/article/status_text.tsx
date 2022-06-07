526d34acce22c98962243621b37c31d633f5001f
aPlikethis <yourhand1025@163.com>
2022-06-07 16:03:39 +0800

    图标更改

--- src/pages/admin/content/article/status_text.tsx
+++ src/pages/admin/content/article/status_text.tsx
@@ -1,12 +1,24 @@
-import { InfoCircleOutlined } from "@ant-design/icons";
+import { CheckCircleOutlined, InfoCircleOutlined } from "@ant-design/icons";
+import { Typography } from "antd";
+
+const { Text } = Typography;
 
 interface Props {
     status: string
 }
 
 function ArtStatusText(props: Props) {
-    const {status} = props;
-    return <InfoCircleOutlined color="" />
+    const { status } = props;
+    return (
+        <>{(() => {
+            if (status == "未启用") {
+                return <Text style={{ color: "#ffa940" }}><InfoCircleOutlined />{status}</Text>
+            }
+            else if (status == "启用") {
+                return <Text style={{ color: "green" }}><CheckCircleOutlined />{status}</Text>
+            }
+        })()}</>
+    )
 }
 
 export default ArtStatusText
